8c363b7e538f2c468606bf90fc4782bc1da0565c
naja <jaco.van.der.nat@pggm.nl>
2021-11-25 15:17:54 +0100

    g5
    uk

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/ReferenceDataModel.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/ReferenceDataModel.java
@@ -1,11 +1,26 @@
 package com.adobe.aem.guides.wknd.core.models;
 
 import com.adobe.cq.wcm.core.components.models.Text;
-import org.osgi.annotation.versioning.ProviderType;
+import com.day.cq.wcm.api.Page;
 
-@ProviderType
-public interface ReferenceDataModel extends Text {
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
 
-    public String getText();
-
+@Model(adaptables = SlingHttpServletRequest.class, adapters = Text.class, resourceType = "wknd/components/referencedata")
+public class ReferenceDataModel implements Text {
+ 
+    @ScriptVariable
+    private Page currentPage;
+ 
+    @Self @Via(type = ResourceSuperType.class)
+    private Text text;
+ 
+    @Override
+    public String getText() {
+        return text.toString();
+    }
 }
